399ffdf701b87dee0f941eeb0d5231d987fbb4b2
denisfl1 <denisfl1@hotmail.com>
2024-02-17 23:06:37 -0300

    updateleet

--- Easy/RemoveDuplicatesfromSortedArray.js
+++ Easy/RemoveDuplicatesfromSortedArray.js
@@ -4,7 +4,7 @@ let nums = [0,0,1,1,1,2,2,3,3,4]
 var removeDuplicates = function(nums) {
     
    const novo = nums.filter((item,index)=>{return nums.indexOf(item) == index})
-    const a = ["1","3"]
+    const a = ["1","3","4"]
     console.log(novo)
 }
 
