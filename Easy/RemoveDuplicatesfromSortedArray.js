621b61ac945b04a903d8d582d8deb0015900effd
denisfl1 <denisfl1@hotmail.com>
2024-02-19 20:38:54 -0300

    update

--- Easy/RemoveDuplicatesfromSortedArray.js
+++ Easy/RemoveDuplicatesfromSortedArray.js
@@ -2,27 +2,20 @@
 let nums = [0,0,1,1,1,2,2,3,3,4]
 
 var removeDuplicates = function(nums) {
-    let positions = new Map()
-    let arr = []
 
-    for(i in nums){
-        let val = nums[i]
-          
-        if(!arr.includes(val)){     
-            arr.push(val)
+    let index = 1
+
+    for(let i= 0;i<nums.length - 1;i++){
+
+        if(nums[i]!==nums[i+1]){
+            nums[index] = nums[i+1]
+            index++
         }
-
-
-        // if(!positions.has(val)){
-        //     positions.set(val,val)
-        //     arr.push(positions.get(val))
-           
-        // }
-      
-       
+        
+    
     }
 
-    console.log(arr)
+    return index
 }
 
 removeDuplicates(nums)
