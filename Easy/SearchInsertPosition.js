455403b5378a21d01e38c463a49db9c605ee223f
denisfl1 <denisfl1@hotmail.com>
2024-02-20 23:49:02 -0300

    leet

--- Easy/SearchInsertPosition.js
+++ Easy/SearchInsertPosition.js
@@ -1,17 +1,22 @@
-let nums = [1,3,5,6], target = 5
+let nums = [1,3,5,6], target = 3
+
 
 var searchInsert = function(nums, target) {
-    let index = 0
+    let n = 0
 
     for(let i = 0;i<nums.length;i++){
 
-        if(nums[i] === target){
-            index = i
+        if(!nums.includes(target)){
+        nums.push(target)
+        nums.sort((a,b)=>{return a-b})
+        
         }
-
+        console.log(nums[i])
+        // console.log(index)
     }
-
-    console.log(index)
+  
+    
+ 
 };
 
 
