26c554558b157782f2ac76acc445fc2cd876af98
denisfl1 <denisfl1@hotmail.com>
2024-02-28 23:12:45 -0300

    update

--- Easy/ConvertSortedArraytoBinarySearchTree.js
+++ Easy/ConvertSortedArraytoBinarySearchTree.js
@@ -26,14 +26,16 @@
 
 var sortedArrayToBST = function(nums) {
     
-    if (!nums.length) return null;
+    if (!nums.length) return null
     
-    const mid = Math.floor(nums.length / 2);
-    const root = new TreeNode(nums[mid]);
+    const mid = Math.floor(nums.length / 2)
+    const root = new TreeNode(nums[mid])
     
-    root.left = sortedArrayToBST(nums.slice(0, mid));
-    root.right = sortedArrayToBST(nums.slice(mid + 1));
+    root.left = sortedArrayToBST(nums.slice(0, mid))
+    root.right = sortedArrayToBST(nums.slice(mid + 1))
     
     return root
 
-};
+}
+let nums = [-10,-3,0,5,9]
+sortedArrayToBST(nums)
