59df6c86e2e2b6580a8da5264f5f04da1d51b284
denisfl1 <denisfl1@hotmail.com>
2024-02-29 20:29:22 -0300

    update

---
+++ Easy/Pascal'sTriangle.js
@@ -0,0 +1,20 @@
+
+// Dado um número inteiro numRows, retorne o primeiro numRows do triângulo de Pascal.
+
+// No triângulo de Pascal, cada número é a soma dos dois números diretamente acima dele, conforme mostrado:
+
+
+// Exemplo 1:
+
+// Entrada: numRows = 5
+// Saída: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+// Exemplo 2:
+
+// Entrada: numRows = 1
+// Saída: [[1]]
+
+var generate = function(numRows) {
+    
+    
+
+};
