eec12f56b1255d1d719ec31d1f0053c212b838ae
denisfl1 <denisfl1@hotmail.com>
2024-03-01 11:00:11 -0300

    update

--- Easy/Pascal'sTriangle.js
+++ Easy/Pascal'sTriangle.js
@@ -6,15 +6,41 @@
 
 // Exemplo 1:
 
-// Entrada: numRows = 5
+//  numRows = 5
 // Saída: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
 // Exemplo 2:
 
 // Entrada: numRows = 1
 // Saída: [[1]]
 
+
+/* Saída: [
+           [1]
+          [1,1],
+         [1,2,1],
+        [1,3,3,1]
+       [1,4,6,4,1]
+
+]  
+
+*/
+
+
+let numRows = 5
 var generate = function(numRows) {
+
+    let ans = []
+    let numRows = 5
     
-    
+    for (let i = 0; i < numRows; i++) {
+        let row = new Array(i + 1).fill(1);
+        for (let j = 1; j < i; j++) {
+            row[j] = ans[i - 1][j] + ans[i - 1][j - 1];
+        }
+        ans.push(row);
+     
+    }
+    console.log (row)
+
 
 };
