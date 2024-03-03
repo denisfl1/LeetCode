d5448074698bf0d6d27a4ff4aba59595df9ed96e
denisfl1 <denisfl1@hotmail.com>
2024-03-03 00:42:53 -0300

    update

---
+++ Easy/PascalsTriangle.rb
@@ -0,0 +1,25 @@
+
+
+numRows = 5
+def generate (numRows)
+
+    ans = []
+
+    for i in 0...(numRows)
+         row = Array.new(i + 1).fill(1)
+
+       for j in 1...(i)
+
+            row[j] = ans[i - 1][j] + ans[i - 1][j - 1]
+           puts row[j]
+        end
+        ans.push(row)
+
+      end
+
+
+
+  end
+
+
+generate(numRows)
