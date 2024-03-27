3101f16ba571e5a555c54fd137fa70783f7eeb91
denisfl1 <denisfl1@hotmail.com>
2024-03-26 23:29:42 -0300

    update

---
+++ Easy/IntersectionofTwoArraysII.js
@@ -0,0 +1,39 @@
+
+
+// Dadas duas matrizes inteiras nums1 e nums2, retorne uma matriz de sua interseção. 
+// Cada elemento no resultado deve aparecer quantas vezes for mostrado em ambos os arrays e você pode retornar o resultado em qualquer ordem.
+
+ 
+
+// Exemplo 1:
+
+// Entrada: nums1 = [1,2,2,1], nums2 = [2,2]
+// Saída: [2,2]
+// Exemplo 2:
+
+// Entrada: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+// Saída: [4,9]
+// Explicação: [9,4] também é aceito.
+ 
+
+// Restrições:
+
+// 1 <= nums1.comprimento, nums2.comprimento <= 1000
+// 0 <= nums1[i], nums2[i] <= 1000
+
+const x  = [1]
+
+var intersect = function(nums1,nums2) {
+    
+    let arrA = new Map(nums1)
+    let arrB = new Map(nums2)
+    let arr  = []
+
+    for(const i of arrA){
+        if(arrB.has(i))arr.push(i)
+    }
+
+    console.log(arr)
+}
+
+intersect([1,2,2,1],[2,2])
