ab1dcbb57181f0edd64b679f90fb7f42107f2106
denisfl1 <denisfl1@hotmail.com>
2024-04-02 20:09:52 -0300

    update

--- Easy/IslandPerimeter.rb
+++ Easy/IslandPerimeter.rb
@@ -1,27 +1,27 @@
 
-grid = [[1,1]]
+grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
 
 def islandPerimeter(grid)
 
   perimeter = 0
 
-    for i in grid.size
+    for i in 0...grid.size
 
-      for j in grid[i].size
+      for j in 0...grid[i].size
 
-        if(grid[i][j] == 1)
-          perimeter+=4
+            if(grid[i][j] == 1)
+                perimeter+=4
 
-        if(i > 0 && grid[i - 1][j]== 1)
-          perimeter-=2
-        end
+                  if(i > 0 && grid[i - 1][j]== 1)
+                    perimeter-=2
+                  end
 
-        if(j > 0 && grid[i][j - 1] == 1)
-          perimeter-=2
-        end
+                  if(j > 0 && grid[i][j - 1] == 1)
+                    perimeter-=2
+                  end
 
+            end
       end
-
 
   end
 
