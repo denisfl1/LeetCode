e8f26c3e2b649e422ae2d5eb7c4dbf79f4c580a4
denisfl1 <denisfl1@hotmail.com>
2024-04-09 17:15:35 -0300

    update

--- Easy/KeyboardRow.rb
+++ Easy/KeyboardRow.rb
@@ -1,33 +1,28 @@
 
-word  = ["adsdf","sfd"]
 
 def findWords(words)
-  line1 = Set.new("qwertyuiop")
-  line2 = Set.new("asdfghjkl")
-  line3 = Set.new("zxcvbnm")
+  line1 = Set.new("qwertyuiop".chars)
+  line2 = Set.new("asdfghjkl".chars)
+  line3 = Set.new("zxcvbnm".chars)
 
   res = []
 
   words.each do |word|
-
-      if compare(word, line1) || compare(word, line2) || compare(word, line3)
-        res.push(word)
-      end
-
+    if compare(word, line1) || compare(word, line2) || compare(word, line3)
+      res.push(word)
+    end
   end
 
   return res
 end
 
 def compare(word, row)
-
   word.downcase.each_char do |char|
     return false unless row.include?(char)
   end
-
   return true
 end
 
-
-
-findWords(word)
+words = ["Hello", "Alaska", "Dad", "Peace"]
+result = findWords(words)
+puts result.inspect
