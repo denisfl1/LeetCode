c894ccdfd9cc4b37ece71e6779148d8729f23924
denisfl1 <denisfl1@hotmail.com>
2024-04-13 23:58:35 -0300

    update

--- Easy/ReshapetheMatrix.js
+++ Easy/ReshapetheMatrix.js
@@ -16,9 +16,20 @@
 
 var matrixReshape = function(mat, r, c) {
  
-    let size  = mat.length
+    let rows = mat.length
+    let collumns = nums[0].length
+    let output = new Array()
 
- 
-    
+        if(rows * collumns != r * c)return rows
 
+        let row_num = 0
+        let collumn_num = 0
+
+        // for(let i = 0;i<rows;i++){
+        //     for(let j =0;j<collumns;j++){
+        //         collumn_num[row_num][collumn_num] = 
+        //     }
+        // }
+
+    const x = [1]
 }
