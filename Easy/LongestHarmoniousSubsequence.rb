5a893421501831b2eaef1c2d996c0f0f11fcc821
denisfl1 <denisfl1@hotmail.com>
2024-04-23 12:52:59 -0300

    update

--- Easy/LongestHarmoniousSubsequence.rb
+++ Easy/LongestHarmoniousSubsequence.rb
@@ -4,17 +4,17 @@
 def findLHS(nums)
 
   ans = 0
-  obj = Hash.new(0)
+  numbers = Hash.new(0)
 
     for i in nums
 
-      obj[i] += 1
+     numbers[i] += 1
 
     end
 
-    obj.each do |key,value |
+   numbers.each do |key,value |
 
-      if obj.include?(key + 1)
+      if numbers.include?(key + 1)
 
         ans = [0,obj[key + 1] + value].max
 
