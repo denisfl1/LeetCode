3d612c001be5b9a0008849bc156043199ad7e896
denisfl1 <denisfl1@hotmail.com>
2024-06-14 23:01:48 -0300

    newLeet

---
+++ Easy/String/Length_of_Last_Word.js
@@ -0,0 +1,30 @@
+
+
+// Dada uma string s que consiste em palavras e espaços, retorne o comprimento da última palavra da string.
+
+// Uma palavra é um máximo
+// substring
+//  consistindo apenas em caracteres sem espaço.
+
+
+
+// Exemplo 1:
+
+// Entrada: s = "Olá mundo"
+// Saída: 5
+// Explicação: A última palavra é “World” com comprimento 5.
+// Exemplo 2:
+
+// Entrada: s = "voe-me para a lua"
+// Saída: 4
+// Explicação: A última palavra é “lua” com comprimento 4.
+// Exemplo 3:
+
+// Entrada: s = "luffy ainda é joyboy"
+// Saída: 6
+// Explicação: A última palavra é “joyboy” com comprimento 6.
+
+
+var lengthOfLastWord = function(s) {
+    
+};
