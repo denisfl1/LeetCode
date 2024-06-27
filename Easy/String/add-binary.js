45de6ca2397099c5934751f7861bf97d2d59fa39
denisfl1 <denisfl1@hotmail.com>
2024-06-27 19:38:36 -0300

    update

--- Easy/String/add-binary.js
+++ Easy/String/add-binary.js
@@ -1,14 +1,4 @@
 
-// Saudações camaradas:
-
-// Abaixo está minha solução chata, mas clara, usando o objeto BigInt proposto.
-
-// A ideia é usar as entradas a e b para construir dois literais binários. 
-// O cálculo da soma é feito chamando a função BigInt em nossos literais binários, 
-// somando-os e retornando a soma com uma chamada ao método toString com 2 como argumento, 
-// já que estamos trabalhando com números binários.
-
-
 
 var addBinary = function(a, b) {
     const aBin = `0b${a}`
