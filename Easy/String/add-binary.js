3fa939bbc4d43a94c91550e2d94ad64f91899a17
denisfl1 <denisfl1@hotmail.com>
2024-06-27 21:07:38 -0300

    update

--- Easy/String/add-binary.js
+++ Easy/String/add-binary.js
@@ -2,6 +2,16 @@
 
 // Dadas duas strings binárias aeb, retorne sua soma como uma string binária.
 
+// Example 1:
+
+// Input: a = "11", b = "1"
+// Output: "100"
+// Example 2:
+
+// Input: a = "1010", b = "1011"
+// Output: "10101"
+ 
+
 var addBinary = function(a, b) {
     const aBin = `0b${a}`
     const bBin = `0b${b}`
