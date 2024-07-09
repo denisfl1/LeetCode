0ecbb1addfe035462d4e519a74cdf2e8c86131c0
denisfl1 <denisfl1@hotmail.com>
2024-07-09 18:03:12 -0300

    new

---
+++ Easy/String/Reverse_String.js
@@ -0,0 +1,26 @@
+
+
+// Escreva uma função que inverta uma string. 
+// A string de entrada é fornecida como uma matriz de caracteres s.
+
+// Você deve fazer isso modificando a matriz de entrada local com memória extra O(1).
+
+// Example 1:
+
+// Input: s = ["h","e","l","l","o"]
+// Output: ["o","l","l","e","h"]
+// Example 2:
+
+// Input: s = ["H","a","n","n","a","h"]
+// Output: ["h","a","n","n","a","H"]
+
+var reverseString = function(s) {
+    
+console.log(s.reverse())
+
+
+}
+
+const s = ["H","a","n","n","a","h"]
+
+reverseString(s)
