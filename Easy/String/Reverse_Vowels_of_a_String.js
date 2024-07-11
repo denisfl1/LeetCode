e9f693d127befbed63e64e44f448d9c992324d3b
denisfl1 <denisfl1@hotmail.com>
2024-07-10 23:17:47 -0300

    continue

---
+++ Easy/String/Reverse_Vowels_of_a_String.js
@@ -0,0 +1,39 @@
+
+
+// Dada uma string s, inverta apenas todas as vogais da string e retorne-a.
+
+// As vogais são 'a', 'e', ​​'i', 'o' e 'u' e podem aparecer em letras maiúsculas e minúsculas, 
+// mais de uma vez.
+
+
+// Example 1:
+
+// Input: s = "hello"
+// Output: "holle"
+// Example 2:
+
+// Input: s = "leetcode"
+// Output: "leotcede"
+
+
+var reverseVowels = function(s) {
+
+    const vogais = ["a","e","i","o","u"]
+    const word = s.split("")
+    let set = new Set(word)
+    
+    let index = 0
+    let string = ''
+
+
+        for(let i = 0;i<word.length;i++){
+            
+                
+        }
+
+
+};
+
+let s =  "leetcode"
+
+reverseVowels(s)
